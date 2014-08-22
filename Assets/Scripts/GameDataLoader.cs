d73ea586565194d6045ad976512d77398fd4e438
nwtnrcks <chulini@gmail.com>
2014-08-22 19:39:05 -0400

    audio fx funcionando, pool

---
+++ Assets/Scripts/GameDataLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameDataLoader : MonoBehaviour {
+	public bool forceReset = false;
+	void Awake(){
+		if(PlayerPrefs.GetInt("isNotFirstTime") != 1 || forceReset){
+			print("Is first time");
+			ResetData();
+		}
+	}
+	public static void ResetData(){
+		Debug.Log("Reset Data!");
+		PlayerPrefs.DeleteAll();
+		PlayerPrefs.SetInt("isNotFirstTime",1);
+		
+	}
+}
