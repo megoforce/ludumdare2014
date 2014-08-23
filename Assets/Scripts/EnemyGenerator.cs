7e75c829f4a4bb3ff810f8d2d45badb06c15e14d
nwtnrcks <chulini@gmail.com>
2014-08-23 19:51:54 -0400

    camera FX

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class EnemyGenerator : MonoBehaviour {
 	public GameObject characterPrefab;
 	public void GenerateEnemies(){
-		GameObject newEnemy = Instantiate(characterPrefab) as GameObject;
-		CharacterProperties cp = newEnemy.GetComponent<CharacterProperties>();
-		cp.Init(true);
-		newEnemy.transform.position = new Vector3(2,2,0);
+		for(int i = 0; i < 10; i++){
+			GameObject newEnemy = Instantiate(characterPrefab) as GameObject;
+			CharacterProperties cp = newEnemy.GetComponent<CharacterProperties>();
+			cp.Init(true);
+			newEnemy.transform.position = new Vector3(2*i,RandomExt.RandomFloatBetween(0,10),0);
+		}
 	}
 }
