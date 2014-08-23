15d5cf040e3fc3f3f6d2028c0c58c0d478c50013
nwtnrcks <chulini@gmail.com>
2014-08-23 02:47:54 -0400

    player with walk animation

--- Assets/Scripts/PlayerAnimations.cs
+++ Assets/Scripts/PlayerAnimations.cs
@@ -11,7 +11,64 @@ public class PlayerAnimations : MonoBehaviour {
 		playerMotor = GetComponent<PlayerMotor>();
 	}
 
+	int fCounter = 0;
 	void Update(){
+		fCounter ++;
+
+
+		if(Mathf.Abs(playerMotor.horizontal) > Mathf.Abs(playerMotor.vertical)){
+			WalkH(playerMotor.horizontal);
+		} else if(Mathf.Abs(playerMotor.horizontal) < Mathf.Abs(playerMotor.vertical)){
+			WalkV(playerMotor.vertical);
+		}
 
 	}
+	/*
+		0 1 2 down
+		3 4 5 left
+		6 7 8 right
+		9 10 11 up
+	 */
+
+	int offsetH = 0;
+
+	void WalkH(float speed){
+		int v = 10 - (int)(Mathf.Abs(speed*8));
+
+		if(speed > 0){
+			sprite.spriteId = 8 - offsetH;
+		} else {
+			sprite.spriteId = 5 - offsetH;
+		}
+
+		if((fCounter % v) == 0){
+			offsetH = (offsetH + 1 == 3) ? 0 : offsetH + 1;
+		}
+	}
+	void WalkV(float speed){
+		int v = 10 - (int)(Mathf.Abs(speed*8));
+		
+		if(speed > 0){
+			sprite.spriteId = 11 - offsetH;
+		} else {
+			sprite.spriteId = 2 - offsetH;
+		}
+		
+		if((fCounter % v) == 0){
+			offsetH = (offsetH + 1 == 3) ? 0 : offsetH + 1;
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
