98d34eab1e1f0c262ae8e96e49a24aae0d4f9663
nwtnrcks <chulini@gmail.com>
2014-08-23 18:23:15 -0400

    execution order

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class EnemyGenerator : MonoBehaviour {
 	public GameObject characterPrefab;
 	public void GenerateEnemies(){
+		GameObject newEnemy = Instantiate(characterPrefab) as GameObject;
+		CharacterProperties cp = GetComponent<CharacterProperties>();
+		cp.AI = true;
+		cp.spriteName = "enemy";
 
 	}
 }
