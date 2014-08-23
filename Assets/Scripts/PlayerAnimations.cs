d51a29d13d383d76e4302541f016b5b6bb0517d5
nwtnrcks <chulini@gmail.com>
2014-08-23 02:05:48 -0400

    motor

---
+++ Assets/Scripts/PlayerAnimations.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerAnimations : MonoBehaviour {
+	public tk2dSprite sprite;
+	PlayerMotor playerMotor;
+
+
+	void Awake(){
+		playerMotor = GetComponent<PlayerMotor>();
+	}
+
+	void Update(){
+
+	}
+}
