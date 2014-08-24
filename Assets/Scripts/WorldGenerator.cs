642162af6157d286d5da5a1723742d850790006c
phaser <phaser@tubatuba.net>
2014-08-23 21:58:15 -0400

    terrain, humidity

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -13,6 +13,14 @@ public class WorldGenerator : MonoBehaviour {
 				tileMap.Layers[0].SetTile(i,j,floorTileId);
 				if(Random.Range(0,300)>200) {
 					tileMap.Layers[1].SetTile(i,j,0);
+					if(Random.Range(0,humidity)>50) {
+						tileMap.Layers[1].SetTile(i,j,7);
+					} 
+					if(Random.Range(0,humidity)<50) {
+						tileMap.Layers[1].SetTile(i,j,1);
+					} 
+
+
 				}
 			}
 		}
@@ -55,36 +63,7 @@ public class WorldGenerator : MonoBehaviour {
 			}
 		}
 
-		// left and right borders
-		for(int x = 0; x < tileMap.width; x++){
-			for(int y = 0; y < tileMap.width; y++){
-				int tileid=tileMap.GetTile(x,y,5);
-				if(tileid==10) {
-					Debug.Log ("x: "+x.ToString()+" y:"+y.ToString());
-					if(x>0 && x<(tileMap.width-1) && y>0 && y<(tileMap.height-1) && y>0) {
-						// top-left
-						if(tileMap.GetTile(x-1,y,5)<0 && tileMap.GetTile(x,y+1,5)<0) {
-							tileMap.Layers[5].SetTile(x,y,3);
-						}
-						// top-right
-						
-						if(tileMap.GetTile(x+1,y,5)<0 && tileMap.GetTile(x,y+1,5)<0) {
-							tileMap.Layers[5].SetTile(x,y,5);
-						}
-						// bottom-right
-						if(tileMap.GetTile(x,y-1,5)==22 && (tileMap.GetTile(x+1,y,5)<0||tileMap.GetTile(x+1,y,5)==22||tileMap.GetTile(x+1,y,5)==28)) {
-							tileMap.Layers[5].SetTile(x,y,17);
-						}
-						// bottom-left
-						if((tileMap.GetTile(x-1,y,5)<0||tileMap.GetTile(x-1,y,5)==22||tileMap.GetTile(x-1,y,5)==28) && tileMap.GetTile(x,y-1,5)==22) {
-							tileMap.Layers[5].SetTile(x,y,15);
-						}
-					}
 
-				}
-
-			}
-		}
 		
 		tileMap.ForceBuild();
 	}
