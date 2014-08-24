96002851d06dd70d85f810dc243b54944505d114
phaser <phaser@tubatuba.net>
2014-08-24 15:30:40 -0400

    audios

---
+++ Assets/AudioTrack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioTrack : MonoBehaviour {
+	public AudioSource wind;
+
+	// Use this for initialization
+	void Start () {
+		float speed = Random.Range (0, 100);
+		wind.pitch = speed*0.0004f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
