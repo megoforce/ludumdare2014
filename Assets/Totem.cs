a4dd5c3f6b7e6875385f36165b72ee962cdf26ca
nwtnrcks <chulini@gmail.com>
2014-08-25 01:41:25 -0400

    askljd

--- Assets/Totem.cs
+++ Assets/Totem.cs
@@ -7,10 +7,6 @@ public class Totem : MonoBehaviour {
 	int totemID = 0;
 	public void Init(int totemID, int x, int y){
 		GetComponent<SpriteRenderer>().sprite = totems[totemID];
-		/*
-		 * 			int x = (int)(transform.position.x*(256f/82f));
-			int y = (int)(transform.position.y*(256f/82f));
-			*/
 
 	}
 
