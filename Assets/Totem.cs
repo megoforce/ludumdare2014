7894325e0b4656d21c2e3a77b2f66b0fca315cb1
nwtnrcks <chulini@gmail.com>
2014-08-25 01:36:10 -0400

    asd

--- Assets/Totem.cs
+++ Assets/Totem.cs
@@ -2,9 +2,12 @@
 using System.Collections;
 
 public class Totem : MonoBehaviour {
+	public Sprite[] totems;
+
 	int totemID = 0;
-	public void Init(int totemID){
-		
+	public void Init(int totemID, int x, int y){
+		GetComponent<SpriteRenderer>().sprite = totems[totemID];
+
 	}
 
 }
