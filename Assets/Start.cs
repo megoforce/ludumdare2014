10d99624092e7c74c2d483f14f8af13c90255632
Nicolas Justiniano <n.justiniano@gmail.com>
2014-08-25 00:18:00 -0400

    intro

---
+++ Assets/Start.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start : MonoBehaviour {
+	public GameObject particles;
+	public GameObject menu;
+	public GameObject intro;
+
+	void Hola() {
+		print("hola");
+		menu.GetComponent<FadeObjectInOut>().FadeOut();
+		particles.SetActive(false);
+		intro.SetActive(true);
+		intro.GetComponent<IntroScript>().playintro=true;
+		//menu.FadeOut();
+		//Application.LoadLevel("SomeLevel");
+	}
+
+}
