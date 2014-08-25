fa7779cc0c72f787a7f3a44c98b7c9c3c0271f9d
nwtnrcks <chulini@gmail.com>
2014-08-24 22:15:04 -0400

    GUI progress

---
+++ Assets/CustomInputData.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class CustomInputData : MonoBehaviour {
+	void Start()
+	{
+		SetCustomInput();
+	}
+	
+	
+	public static void SetCustomInput()
+	{
+		UICamera.onCustomInput = CustomInput;
+	}
+	
+	
+	public static void CustomInput()
+	{
+		if (UICamera.selectedObject == null)
+		{
+			return;
+		}
+		
+		//The default NGUI cancel key is KeyCode.JoystickButton1 and will cause
+		//NGUI to intercept this and set the selected object to null
+		//before we get a chance to interpret the input. Setting it to
+		//KeyCode.None will cause it to ignore that test.
+		UICamera.current.cancelKey1 = KeyCode.None;
+		
+		InputDevice device = InputManager.ActiveDevice;
+		
+		if (device.Action1.WasPressed)
+		{
+			UICamera.selectedObject.SendMessage( "OnClick" );
+		}
+		
+		if (device.Direction.Up.WasPressed)
+		{
+			UICamera.currentScheme = UICamera.ControlScheme.Controller;
+			UICamera.selectedObject.SendMessage( "OnKey", KeyCode.UpArrow );
+		}
+		
+		if (device.Direction.Down.WasPressed)
+		{
+			UICamera.currentScheme = UICamera.ControlScheme.Controller;
+			UICamera.selectedObject.SendMessage( "OnKey", KeyCode.DownArrow );
+		}
+		
+		if (device.Direction.Right.WasPressed)
+		{
+			UICamera.currentScheme = UICamera.ControlScheme.Controller;
+			UICamera.selectedObject.SendMessage( "OnKey", KeyCode.RightArrow );
+		}
+		
+		if (device.Direction.Left.WasPressed)
+		{
+			UICamera.currentScheme = UICamera.ControlScheme.Controller;
+			UICamera.selectedObject.SendMessage( "OnKey", KeyCode.LeftArrow );
+		}
+	}
+}
