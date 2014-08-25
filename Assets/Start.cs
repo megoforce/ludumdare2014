ee2b90c3c8cf2d310333bea72358d3a66b0fb06d
nwtnrcks <chulini@gmail.com>
2014-08-25 01:26:45 -0400

    fixes intro

--- Assets/Start.cs
+++ Assets/Start.cs
@@ -2,18 +2,26 @@
 using System.Collections;
 
 public class Start : MonoBehaviour {
+	public TweenAlpha tween;
 	public GameObject particles;
 	public GameObject menu;
 	public GameObject intro;
+	public GameObject startButton;
 
 	void Hola() {
-		print("hola");
-		menu.GetComponent<FadeObjectInOut>().FadeOut();
+		tween.Play(true);
+
+
+		startButton.SetActive(false);
+		//Application.LoadLevel("SomeLevel");
+	}
+	public void EndFade(){
+		menu.SetActive(false);
 		particles.SetActive(false);
 		intro.SetActive(true);
 		intro.GetComponent<IntroScript>().playintro=true;
 		intro.GetComponent<IntroScript>().startAnimation();
-		//Application.LoadLevel("SomeLevel");
+		tween.gameObject.SetActive(false);
 	}
 
 }
